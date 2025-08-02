fcbb7d185792d68107ebce17276d4745b30ed822
Пройдисвіт <92428846+Kolodieiev@users.noreply.github.com>
2025-08-03 02:10:22 +0300

    Тести будуть перенесені в основну прошивку

--- firmware/hardware_tests/include/modules_setup/lora/LoRa.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <stdint.h>
-
-#define PIN_LR_AUX 8
-#define PIN_LR_TX 18
-#define PIN_LR_RX 17
-#define PIN_LR_M1 16
-#define PIN_LR_M0 15
--- firmware/hardware_tests/src/meow/lib/DS3231/DS3231ComnConst.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <Arduino.h>
-
-const uint8_t DS3231_ADDR{0x68};
-const uint8_t DS3231_REG_CTRL{0x0E};
-const uint8_t DS3231_REG_STATUS{0x0F};
