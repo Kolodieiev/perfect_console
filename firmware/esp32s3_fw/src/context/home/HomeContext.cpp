ee64d2b5f475987fb2e258861910871b11a44404
Пройдисвіт <92428846+Kolodieiev@users.noreply.github.com>
2025-08-19 11:21:10 +0300

    Виправлено імена змінних

--- firmware/esp32s3_fw/src/context/home/HomeContext.cpp
+++ firmware/esp32s3_fw/src/context/home/HomeContext.cpp
@@ -6,8 +6,6 @@
 
 #define UPD_DISPLAY_INTERVAL_MS 5000UL
 
-const char EMPTY_BAT[] = "0.00";
-
 HomeContext::HomeContext()
 {
     WidgetCreator creator;
@@ -28,29 +26,29 @@ HomeContext::HomeContext()
         layout->addWidget(wallpp_img);
     }
 
-    _bat_volt_lbl = new Label(ID_BAT_LVL);
-    layout->addWidget(_bat_volt_lbl);
-    _bat_volt_lbl->setText(EMPTY_BAT);
-    _bat_volt_lbl->setWidth(56);
-    _bat_volt_lbl->setHeight(35);
-    _bat_volt_lbl->setAlign(Label::ALIGN_CENTER);
-    _bat_volt_lbl->setGravity(Label::GRAVITY_CENTER);
-    _bat_volt_lbl->setPos(_display.width() - TFT_CUTOUT - DISPLAY_PADDING - _bat_volt_lbl->getWidth(), DISPLAY_PADDING);
-    _bat_volt_lbl->setTransparency(true);
+    _batt_volt_lbl = new Label(ID_BAT_LVL);
+    layout->addWidget(_batt_volt_lbl);
+    _batt_volt_lbl->setText(STR_EMPTY_BAT);
+    _batt_volt_lbl->setWidth(56);
+    _batt_volt_lbl->setHeight(35);
+    _batt_volt_lbl->setAlign(Label::ALIGN_CENTER);
+    _batt_volt_lbl->setGravity(Label::GRAVITY_CENTER);
+    _batt_volt_lbl->setPos(_display.width() - TFT_CUTOUT - DISPLAY_PADDING - _batt_volt_lbl->getWidth(), DISPLAY_PADDING);
+    _batt_volt_lbl->setTransparency(true);
 
-    _bat_ico = new Image(1);
-    _bat_volt_lbl->setBackImg(_bat_ico);
-    _bat_ico->init(_bat_volt_lbl->getWidth(), _bat_volt_lbl->getHeight());
-    _bat_ico->setSrc(ICO_BATTERY);
-    _bat_ico->setTransparency(true);
-    _bat_ico->setTranspColor(TFT_TRANSPARENT);
+    _batt_ico = new Image(1);
+    _batt_volt_lbl->setBackImg(_batt_ico);
+    _batt_ico->init(_batt_volt_lbl->getWidth(), _batt_volt_lbl->getHeight());
+    _batt_ico->setSrc(ICO_BATTERY);
+    _batt_ico->setTransparency(true);
+    _batt_ico->setTranspColor(TFT_TRANSPARENT);
 
-    updateBatVoltage();
+    updateBattVoltage();
 }
 
 HomeContext::~HomeContext()
 {
-    delete _bat_ico;
+    delete _batt_ico;
     free(_wallpaper_ptr);
 }
 
@@ -70,15 +68,15 @@ void HomeContext::update()
     if (millis() - _upd_timer > UPD_DISPLAY_INTERVAL_MS)
     {
         _upd_timer = millis();
-        updateBatVoltage();
+        updateBattVoltage();
     }
 }
 
-void HomeContext::updateBatVoltage()
+void HomeContext::updateBattVoltage()
 {
     float bat_voltage = BatteryUtil::readVoltVal();
     String volt_str = String(bat_voltage);
-    _bat_volt_lbl->setText(volt_str);
+    _batt_volt_lbl->setText(volt_str);
 
     getLayout()->forcedDraw();
 }
