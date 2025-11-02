9288b9fa271aadbc5fe7ba26bcfbb0863c3bb1e0
Пройдисвіт <92428846+Kolodieiev@users.noreply.github.com>
2025-11-02 14:20:56 +0200

    Видалено непотрібний для платформи код

--- firmware/esp32s3_fw/src/pixeler/defines.h
+++ firmware/esp32s3_fw/src/pixeler/defines.h
@@ -1,26 +1,7 @@
 #pragma once
 
+#include <Arduino.h>
 #include <math.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
-
-#ifdef ESP32
-#include <Arduino.h>
-#else
-#include <chrono>
-#include <functional>
-#include <thread>
-
-#include "String/WString.h"
-
-#define log_e(fmt, ...) printf("E: " fmt "\n", ##__VA_ARGS__)
-#define log_i(fmt, ...) printf("I: " fmt "\n", ##__VA_ARGS__)
-#define esp_restart() exit(1)
-#define delay(ms) std::this_thread::sleep_for(std::chrono::milliseconds(ms))
-
-#define millis() (std::chrono::duration_cast<std::chrono::milliseconds>(   \
-                      std::chrono::steady_clock::now().time_since_epoch()) \
-                      .count())
-
-#endif  // #ifdef ESP32
