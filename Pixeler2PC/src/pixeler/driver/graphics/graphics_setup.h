fea041361a0d808cc61741db10b00307783deb03
Пройдисвіт <92428846+Kolodieiev@users.noreply.github.com>
2025-11-04 21:26:50 +0200

    Видалено обмеження FPS

--- Pixeler2PC/src/pixeler/driver/graphics/graphics_setup.h
+++ Pixeler2PC/src/pixeler/driver/graphics/graphics_setup.h
@@ -5,7 +5,6 @@
 #define TFT_HEIGHT 320      // Висота дисплею.
 #define GRAPHICS_ENABLED    // Увімкнути підтримку графічного драйвера. Закоментуй, якщо в проєкті не використовується дисплей.
 #define UI_UPDATE_DELAY 25  // Затримка (мс) між фреймами
-#define SFML_FRAME_RATE_LIMIT 60
 #define DISPLAY_ROTATION 0  // Стартова орієнтація екрану
 
 // #define SHOW_FPS             // Відображати значення FPS на дисплеї
