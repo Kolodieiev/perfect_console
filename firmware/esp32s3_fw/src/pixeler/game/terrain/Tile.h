38aee5ab8bb9a5285bbd29f9331c75781aaa4ec6
Пройдисвіт <92428846+Kolodieiev@users.noreply.github.com>
2025-11-13 17:42:42 +0200

    Розширено тип маски прохідності спрайту

--- firmware/esp32s3_fw/src/pixeler/game/terrain/Tile.h
+++ firmware/esp32s3_fw/src/pixeler/game/terrain/Tile.h
@@ -7,7 +7,7 @@ namespace pixeler
   {
   public:
     // Типи поверхні плитки. Можете розширити власними типами, за потреби.
-    enum TileType : uint8_t
+    enum TileType : uint16_t
     {
       TYPE_NONE = 0,
       TYPE_WALL = 1 << 0,
@@ -16,8 +16,11 @@ namespace pixeler
       TYPE_AIR = 1 << 3,
       TYPE_FIRE = 1 << 4,
       TYPE_SNOW = 1 << 5,
-      TYPE_MUD = 1 << 6
+      TYPE_MUD = 1 << 6,
+      TYPE_ROAD = 1 << 7,
+      TYPE_TREE = 1 << 8
     };
+
 
     Tile(TileType type, const uint16_t* img_ptr) : _img_ptr{img_ptr}, _type{type} {}
 
