ad7e0322ce0cf95e5a12fd6455aef835ff54da8b
Пройдисвіт <valerij.kolodeev@gmail.com>
2026-01-17 18:56:18 +0200

    Переміщено в namespace

--- firmware/esp32s3_fw/src/pixeler/setup/coprocessor_setup.h
+++ firmware/esp32s3_fw/src/pixeler/setup/coprocessor_setup.h
@@ -5,16 +5,19 @@
 
 #ifdef HAS_COPROCESSOR
 
-typedef enum : uint8_t  // Перечислення містить список команд для керування іншими МК по шині I2C.
+namespace pixeler
 {
-  CCPU_CMD_ENABLE = 0,
-  CCPU_CMD_DISABLE,
-  CCPU_CMD_PIN_ON,
-  CCPU_CMD_PIN_OFF,
-  CCPU_CMD_GET_BTNS_STATE,
-  CCPU_CMD_BTN_OFF,
-  CCPU_CMD_BTN_ON,
-} CoprocessorCMD_t;
+  typedef enum : uint8_t  // Перечислення містить список команд для керування іншими МК по шині I2C.
+  {
+    CCPU_CMD_ENABLE = 0,
+    CCPU_CMD_DISABLE,
+    CCPU_CMD_PIN_ON,
+    CCPU_CMD_PIN_OFF,
+    CCPU_CMD_GET_BTNS_STATE,
+    CCPU_CMD_BTN_OFF,
+    CCPU_CMD_BTN_ON,
+  } CoprocessorCMD_t;
+}  // namespace pixeler
 
 #define COPROCESSOR_ADDR 0x66  // I2C - адреса МК, який відповідає за розширення функціоналу основного.
 
