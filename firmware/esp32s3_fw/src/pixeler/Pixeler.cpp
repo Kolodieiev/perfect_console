6a24c124e6116592026dc846ca36ad38af08c729
Пройдисвіт <valerij.kolodeev@gmail.com>
2026-01-19 19:34:19 +0200

    Видалено надмірне розділення на класи

--- firmware/esp32s3_fw/src/pixeler/Pixeler.cpp
+++ firmware/esp32s3_fw/src/pixeler/Pixeler.cpp
@@ -1,6 +1,11 @@
 #include "Pixeler.h"
 
-#include "manager/ContextManager.h"
+#include "pixeler/driver/graphics/DisplayWrapper.h"
+#include "pixeler/manager/SPI_Manager.h"
+#include "pixeler/setup/context_id_setup.h"
+#include "pixeler/setup/display_setup.h"
+#include "pixeler/setup/ui_setup.h"
+#include "pixeler/ui/context/IContext.h"
 #include "setup/cpu_setup.h"
 
 namespace pixeler
@@ -14,7 +19,43 @@ namespace pixeler
   {
     setCpuFrequencyMhz(BASE_CPU_FREQ_MHZ);
 
-    ContextManager context_manager;
-    context_manager.run();
+    _input.__init();
+
+#ifdef GRAPHICS_ENABLED
+    _display.__init();
+#endif
+
+    IContext* context = new START_CONTEXT();
+
+    unsigned long ts = millis();
+    while (1)
+    {
+      if (!context->isReleased())
+      {
+        context->tick();
+      }
+      else
+      {
+        ContextID next_context_id = context->getNextContextID();
+        delete context;
+
+        const auto it = _context_id_map.find(next_context_id);
+        if (it == _context_id_map.end())
+        {
+          log_e("Невідомий ідентифікатор контексту: %u", next_context_id);
+          esp_restart();
+        }
+        else
+        {
+          context = it->second();
+        }
+      }
+
+      if (millis() - ts > WDT_GUARD_TIME)
+      {
+        delay(1);
+        ts = millis();
+      }
+    }
   }
 }  // namespace pixeler
