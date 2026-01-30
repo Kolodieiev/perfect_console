61420547297ebd5d7c365a9dcfd6ee3c844ea728
Пройдисвіт <valerij.kolodeev@gmail.com>
2026-01-31 01:20:52 +0200

    Оновлено розмір буферів сервера

--- firmware/esp32s3_fw/src/pixeler/lib/server/FServer/setup.h
+++ firmware/esp32s3_fw/src/pixeler/lib/server/FServer/setup.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#define HTTP_DOWNLOAD_UNIT_SIZE 3072
-#define HTTP_UPLOAD_BUFLEN 3072
-#define HTTP_RAW_BUFLEN 3072
+#define HTTP_DOWNLOAD_UNIT_SIZE 4096
+#define HTTP_UPLOAD_BUFLEN 4096
+#define HTTP_RAW_BUFLEN 4096
 
 #define HTTP_MAX_DATA_WAIT 5000   // ms to wait for the client to send the request
 #define HTTP_MAX_POST_WAIT 5000   // ms to wait for POST data to arrive
